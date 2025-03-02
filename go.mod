a4b4ce08dde4e96fe5d457189c5c71149bb1d1cb
chrisis <chrisis.zann@gmail.com>
2025-03-02 22:14:52 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module fsmdemo
+
+go 1.21.0
---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type fsmVar int
+
+const (
+	success fsmVar = iota
+	failure
+)
+
+type fsmState int
+
+const (
+	s1 fsmState = iota
+	s2
+	s3
+)
+
+func (f *fsm) StartFSM() {
+	for {
+
+		select {
+		case v := <-f.v1:
+
+			switch f.curState {
+			case s1:
+				switch v {
+				case success:
+					f.nxtState = s2
+				case failure:
+					f.nxtState = s3
+
+				}
+			case s2:
+				switch v {
+				case success:
+					f.nxtState = s3
+				case failure:
+					f.nxtState = s1
+
+				}
+			case s3:
+				switch v {
+				case success:
+					f.nxtState = s1
+				case failure:
+					f.nxtState = s2
+				}
+			}
+
+		}
+		f.curState = f.nxtState
+		fmt.Println("New state", f.curState)
+	}
+}
+
+type fsm struct {
+	v1       chan fsmVar
+	curState fsmState
+	nxtState fsmState
+}
+
+func main() {
+	fmt.Println("Starting ...")
+
+	f := fsm{v1: make(chan fsmVar),
+		curState: s1,
+		nxtState: s2,
+	}
+
+	go f.StartFSM()
+
+	var user string
+	for {
+		fmt.Print("Enter message:")
+		fmt.Scan(&user)
+		if strings.Compare(user, "yes") == 0 {
+			f.v1 <- success
+		} else {
+			f.v1 <- failure
+		}
+	}
+}
